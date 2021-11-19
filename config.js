39c6bd0275cfe7ed3c57c04265aec264f92eafa2
Wolyo <93871074+Wolyo-JS@users.noreply.github.com>
2021-11-19 15:47:49 +0400

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,20 @@ OwnerID: "810161653756198933", // sahip id
 GuildID: "905520135819890729", // sunucu id
 VoiceID: "906495246970531840", // botun gireceği ses kanal id
 Token: "", // tokenin
-MongoURL: "",  // mongo urlsi girmezsen bot çapışmaz
+MongoURL: "mongodb+srv://aqua:aqua@aqua.muhdt.mongodb.net/aqua?retryWrites=true&w=majority",  // mongo urlsi girmezsen bot çapışmaz
 Prefix: ["w!"], // istediğiniz kadar çoğaltabilirsini,
 
-GuardBots: [""], // koruma botlarının idleri
-BotRoles: [""], // diğer botların idleri
-JailRole: "", // jail rol
-BoosterRole: "", // booster rol
+GuardBots: ["907303829425487952","907304001832357948","907304249506005063"], // koruma botlarının idleri
+BotRoles: ["908771843522035823","908772374592258118","908772906044125234","908774127849058314","908774437023797268","879069533585932328","907304427994611763","869552972839931924","877866290088382504","884857217994608681"], // diğer botların idleri
+JailRole: "906858221245968404", // jail rol
+BoosterRole: "906947437527191602", // booster rol
 
 RolLog: "906943331764957185", // rol log işte
 
 BildirimKanalı: "906570500183035944", // log kanalı 
 
 
-ServerMessage: "", // bot durum
+ServerMessage: "Wars ❤ Wolyo", // bot durum
 ServerCase: "online", // dnd idle online 
-Footer: "Wars" // footer 
+Footer: "Wars ❤ Wolyo" // footer 
 }
