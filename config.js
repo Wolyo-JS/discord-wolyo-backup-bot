610599fd51f553224d3c1fe50a1ced83036c2d5a
Wolyo <93871074+Wolyo-JS@users.noreply.github.com>
2021-11-19 15:20:18 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
-OwnerID: "", // sahip id
-GuildID: "", // sunucu id
-VoiceID: "", // botun gireceği ses kanal id
+OwnerID: "810161653756198933", // sahip id
+GuildID: "905520135819890729", // sunucu id
+VoiceID: "906495246970531840", // botun gireceği ses kanal id
 Token: "", // tokenin
 MongoURL: "",  // mongo urlsi girmezsen bot çapışmaz
-Prefix: ["!", "."], // istediğiniz kadar çoğaltabilirsini,
+Prefix: ["w!"], // istediğiniz kadar çoğaltabilirsini,
 
 GuardBots: [""], // koruma botlarının idleri
 BotRoles: [""], // diğer botların idleri
 JailRole: "", // jail rol
 BoosterRole: "", // booster rol
 
-RolLog: "", // rol log işte
+RolLog: "906943331764957185", // rol log işte
 
-BildirimKanalı: "", // log kanalı 
+BildirimKanalı: "906570500183035944", // log kanalı 
 
 
 ServerMessage: "", // bot durum
-ServerCase: "", // dnd idle online 
-Footer: "" // footer 
+ServerCase: "online", // dnd idle online 
+Footer: "Wars" // footer 
 }
